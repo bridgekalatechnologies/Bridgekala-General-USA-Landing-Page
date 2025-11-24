d45bfe7fd27e0154b9371b9a6858e4424cbecd32
thenikhilsingh <nikhilsingh26042004@gmail.com>
2025-11-24 15:12:24 +0530

    performance section done

---
+++ src/components/Performancesection.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Performancesection() {
+  const cards = [
+    "4.3x Avg Roas",
+    "33 Days avg. time to profitability",
+    "33 Days avg. time to profitability",
+  ];
+  return (
+    <div className="bg-[#9000FF] w-screen p-10 rounded-lg Montserratfont my-5">
+      <div className="w-full bg-white rounded-lg flex flex-col justify-center p-3 gap-3">
+        <h1 className="text-[12px] font-medium text-center">
+          The Only Numbers That Matter
+        </h1>
+        {cards.map((card, i) => {
+          return (
+            <div
+              key={i}
+              className="bg-[#9000FF] text-white flex justify-center items-center text-center p-5 rounded-lg h-30 font-semibold w-full"
+            >
+              <h1 className="text-2xl">{card}</h1>
+            </div>
+          );
+        })}
+       <div>
+         <p className="text-center text-[10px] font-semibold">REAL PERFORMANCE. NOT PROMISES.</p>
+        <p className="text-center text-[8px] font-medium">(Updated 1 Jun, 2025)</p>
+       </div>
+      </div>
+    </div>
+  );
+}
